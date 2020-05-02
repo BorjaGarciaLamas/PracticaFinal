b45673f7191507da17c443b17ec272334fccff06
IMoll <nachomollamoros@gmail.com>
2020-05-02 23:14:21 +0200

    Sigo trabajando en las pestañas

--- practicafinalDIS/src/main/java/com/dis/practicafinal/practicafinalDIS/MyUI.java
+++ practicafinalDIS/src/main/java/com/dis/practicafinal/practicafinalDIS/MyUI.java
@@ -21,6 +21,7 @@ import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.TabSheet.SelectedTabChangeEvent;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -35,95 +36,180 @@ import com.vaadin.ui.VerticalLayout;
 @Theme("mytheme")
 public class MyUI extends UI {
 
+	
+	//Inicialización de variables
 	Lista agenda = new Lista();
 	
+	//Web de Vaadin
     @Override
     protected void init(VaadinRequest vaadinRequest) {
     	
+    	//Vista general.
         final VerticalLayout layout = new VerticalLayout();
         
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
+        //------------ELEMENTOS PRINCIPALES---------------------
         
-        /*
-        //Tabs
-        Tab tab1 = new Tab("Tab one");
-        Div page1 = new Div();
-        page1.setText("Page#1");
-
-        Tab tab2 = new Tab("Tab two");
-        Div page2 = new Div();
-        page2.setText("Page#2");
-        page2.setVisible(false);
-
-        Tab tab3 = new Tab("Tab three");
-        Div page3 = new Div();
-        page3.setText("Page#3");
-        page3.setVisible(false);
-
-        Map<Tab, Component> tabsToPages = new HashMap<>();
-        tabsToPages.put(tab1, page1);
-        tabsToPages.put(tab2, page2);
-        tabsToPages.put(tab3, page3);
-        Tabs tabs = new Tabs(tab1, tab2, tab3);
-        Div pages = new Div(page1, page2, page3);
-        Set<Component> pagesShown = Stream.of(page1)
-                .collect(Collectors.toSet());
-
-        
-    	*/
-        
+        //Pestañas (tabs)
         TabSheet tabsheet = new TabSheet();
-
-        // Create the first tab
+        
         VerticalLayout tab1 = new VerticalLayout();
         VerticalLayout tab2 = new VerticalLayout();
         VerticalLayout tab3 = new VerticalLayout();
         VerticalLayout tab4 = new VerticalLayout();
-
-        tab1.addComponents(name, button);
-        tab2.addComponents(button);
-        tab3.addComponents(name);
-        tab4.addComponents(button);
+        VerticalLayout tab5 = new VerticalLayout();
         
         tabsheet.addTab(tab1, "Overview");
         tabsheet.addTab(tab2, "Añadir");
         tabsheet.addTab(tab3, "Modificar");
         tabsheet.addTab(tab4, "Borrar");
+        tabsheet.addTab(tab5, "Configuración (Gson)");
         
-        //Evento de cambiar pestaña.
-        tabsheet.addSelectedTabChangeListener(event -> {
-        	// Find the tabsheet
-            TabSheet tabsheetActual = event.getTabSheet();
-
-            // Find the tab (here we know it's a layout)
-            Layout tab = (Layout) tabsheetActual.getSelectedTab();
-
-            // Get the tab caption from the tab object
-            String caption = tabsheet.getTab(tab).getCaption();
-
-            // Fill the tab content
-            tab.removeAllComponents();
-        	
+        //Botón 1
+        Button button2 = new Button("Click Me2");
+        button2.addClickListener(e -> {
+            layout.addComponent(new Label("Thanks, it works!"));
         });
-      
-        //Grid e impresión de layout.
+        
+        //------------FIN DE ELEMENTOS PRINCIPALES--------------
+        
+        
+        
+        //------------------PESTAÑA OVERVIEW---------------------
+        
+        //Objeto 1
+        final TextField name1 = new TextField();
+        name1.setCaption("Pestaña 1");
+        
+        //Grid
         Grid<Usuario> grid = new Grid<>(Usuario.class);
         grid.setItems(agenda.getContactos());
         grid.setColumns("nombre", "ape", "dir", "empresa", "mail", "numero");
         
-        //Elementos en el layout.
-        layout.addComponents(tabsheet, grid);
+        //Cargamos los elementos en la pestaña asociada.
+        tab1.addComponents(grid);
         
+        //----------------FIN DE PESTAÑA DE OVERVIEW-------------------
+        
+        
+        
+        //------------------PESTAÑA CREAR NUEVO USUARIO---------------------
+        
+        //Objeto 1
+        final TextField name2 = new TextField();
+        name2.setCaption("Pestaña 2");
+        
+        //Objeto 2
+        Button button = new Button("Click Me");
+
+        
+        //Cargamos los elementos en la pestaña asociada.
+        tab2.addComponents(name2, button);
+        
+        //----------------FIN DE PESTAÑA DE CREAR USUARIO-------------------
+        
+
+        
+        //------------------PESTAÑA CREAR MODIFICAR USUARIO---------------------
+        
+        //Objeto 1
+        final TextField name3 = new TextField();
+        name3.setCaption("Pestaña 3");
+        
+        //Cargamos los elementos en la pestaña asociada.
+        tab3.addComponents();
+        
+        //----------------FIN DE PESTAÑA DE MODIFICAR USUARIO-------------------
+        
+        
+        
+        
+        //------------------PESTAÑA DE BORRAR USUARIO---------------------
+        
+        //Objeto 1
+        final TextField name4 = new TextField();
+        name4.setCaption("Pestaña 4");
+        
+        //Cargamos los elementos en la pestaña asociada.
+        tab4.addComponents();
+        
+        //----------------FIN DE PESTAÑA DE BORRAR USUARIO-------------------
+        
+        
+        
+        
+        //------------------PESTAÑA GSON---------------------
+        //Objeto 1
+        final TextField name5 = new TextField();
+        name5.setCaption("Pestaña 5");
+        
+        //Cargamos los elementos en la pestaña asociada.
+        tab5.addComponents();
+        
+        //----------------FIN DE PESTAÑA GSON-------------------
+
+      
+     
+        
+        //-----------EVENTOS-------------
+        
+        //Evento botón Crear Usuario
+
+        
+        //Evento de cambiar pestaña.
+        tabsheet.addSelectedTabChangeListener(new TabSheet.SelectedTabChangeListener() {
+            public void selectedTabChange(SelectedTabChangeEvent event) {
+	        	//Cogemos el tab actual
+	            TabSheet tabsheetActual = event.getTabSheet();
+	            //Cogemos el layout
+	            Layout tab = (Layout) tabsheetActual.getSelectedTab();
+	            //Cogemos el caption
+	            String caption = tabsheetActual.getTab(tab).getCaption();
+	            System.out.println(caption);
+	            //Eliminamos todos los objetos de la pestaña (en pantalla).
+	            tab.removeAllComponents();
+	            
+	            switch(caption) {
+	            case "Overview":
+	            	System.out.println(tab2.getComponentCount());
+	            	for(int i = 0; i<tab1.getComponentCount(); i++) 
+			            tab.addComponents(tab1.getComponent(i));
+	            	break;
+	            case "Añadir":
+	            	for(int i = 0; i<tab2.getComponentCount(); i++) 
+			            tab.addComponents(tab2.getComponent(i));		
+	            	break;
+	            case "Modificar":
+	            	for(int i = 0; i<tab3.getComponentCount(); i++) 
+			            tab.addComponents(tab3.getComponent(i));
+	            	break;
+	            case "Borrar":
+	            	for(int i = 0; i<tab4.getComponentCount(); i++) 
+			            tab.addComponents(tab4.getComponent(i));
+	            	break;
+	            case "Configuración (Gson)":
+	            	for(int i = 0; i<tab5.getComponentCount(); i++) 
+			            tab.addComponents(tab5.getComponent(i));
+	            	break;
+	            default:
+	            	break;
+	            
+	            }
+            }
+        });
+        //-----------FIN DE EVENTOS-------------
+        
+        //----------CARGA DEL LAYOUT------------------
+        //Ponemos los elementos del layout (siempre visibles)
+        layout.addComponents(tabsheet);
+        
+        //Ponemos los elementos de la pestaña que queremos cargar (tab1).
+        layout.addComponents();
+
         setContent(layout);
+        //----------FIN DE CARGA DEL LAYOUT------------------
         
-        agenda.mostrarLista();   
+        //agenda.mostrarLista();   
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
