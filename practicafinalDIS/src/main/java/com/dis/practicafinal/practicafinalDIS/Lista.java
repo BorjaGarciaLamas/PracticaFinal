6b6d176f0a9b304b23cf0edace503ed845c45ef5
CarlosTendero <carlitosten96@gmail.com>
2020-05-02 18:29:39 +0200

    FuncionalidadLista

--- practicafinalDIS/src/main/java/com/dis/practicafinal/practicafinalDIS/Lista.java
+++ practicafinalDIS/src/main/java/com/dis/practicafinal/practicafinalDIS/Lista.java
@@ -70,7 +70,7 @@ public class Lista {
 		
 		int posicion = -1;
 		
-		for(int i = 0; i < this.tamanyo() && posicion == -1; i++) {
+		for(int i = 0; i < this.tamanyo(); i++) {
 			if(this.contactos.get(i) == u)
 				posicion = i;
 		}
