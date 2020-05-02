e7505fea87d24165f9d94e4c37fada720a363f62
anekom <andreeiitaa100@gmail.com>
2020-05-02 13:32:29 +0200

    heroku

--- practicafinalDIS/src/main/java/com/dis/practicafinal/practicafinalDIS/MyUI.java
+++ practicafinalDIS/src/main/java/com/dis/practicafinal/practicafinalDIS/MyUI.java
@@ -1,7 +1,5 @@
 package com.dis.practicafinal.practicafinalDIS;
 
-import java.util.Collection;
-
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
@@ -9,19 +7,18 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-/*
+/**
  * This UI is the application entry point. A UI may either represent a browser window 
  * (or tab) or some part of an HTML page where a Vaadin application is embedded.
  * <p>
  * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.*/
-
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
 @Theme("mytheme")
 public class MyUI extends UI {
 
@@ -39,21 +36,14 @@ public class MyUI extends UI {
             layout.addComponent(new Label("Thanks " + name.getValue() 
                     + ", it works!"));
         });
-        
-        Grid<Usuario> grid = new Grid<>(Usuario.class);
-    	grid.setItems((Collection<Usuario>) agenda);
 
-    	grid.setColumns("nombre", "ape", "dir", "empresa", "mail", "numero");
-
-        layout.addComponents(name, button, grid);
+        layout.addComponents(name, button);
 
         setContent(layout);
 
 
         agenda.MostrarLista();
     }
-    
-
 
     @WebServlet(urlPatterns = "/", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
