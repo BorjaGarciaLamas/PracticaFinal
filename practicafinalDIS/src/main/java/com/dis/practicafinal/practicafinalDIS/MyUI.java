63cf4948034345a204d1657487c0aab37e56d3ad
IMoll <nachomollamoros@gmail.com>
2020-05-02 20:37:32 +0200

    Añadiendo pesta´ñas (sin terminar)

--- practicafinalDIS/src/main/java/com/dis/practicafinal/practicafinalDIS/MyUI.java
+++ practicafinalDIS/src/main/java/com/dis/practicafinal/practicafinalDIS/MyUI.java
@@ -1,14 +1,26 @@
 package com.dis.practicafinal.practicafinalDIS;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Layout;
+import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -27,6 +39,7 @@ public class MyUI extends UI {
 	
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+    	
         final VerticalLayout layout = new VerticalLayout();
         
         final TextField name = new TextField();
@@ -38,15 +51,77 @@ public class MyUI extends UI {
                     + ", it works!"));
         });
         
+        /*
+        //Tabs
+        Tab tab1 = new Tab("Tab one");
+        Div page1 = new Div();
+        page1.setText("Page#1");
+
+        Tab tab2 = new Tab("Tab two");
+        Div page2 = new Div();
+        page2.setText("Page#2");
+        page2.setVisible(false);
+
+        Tab tab3 = new Tab("Tab three");
+        Div page3 = new Div();
+        page3.setText("Page#3");
+        page3.setVisible(false);
+
+        Map<Tab, Component> tabsToPages = new HashMap<>();
+        tabsToPages.put(tab1, page1);
+        tabsToPages.put(tab2, page2);
+        tabsToPages.put(tab3, page3);
+        Tabs tabs = new Tabs(tab1, tab2, tab3);
+        Div pages = new Div(page1, page2, page3);
+        Set<Component> pagesShown = Stream.of(page1)
+                .collect(Collectors.toSet());
+
         
+    	*/
+        
+        TabSheet tabsheet = new TabSheet();
+
+        // Create the first tab
+        VerticalLayout tab1 = new VerticalLayout();
+        VerticalLayout tab2 = new VerticalLayout();
+        VerticalLayout tab3 = new VerticalLayout();
+        VerticalLayout tab4 = new VerticalLayout();
+
+        tab1.addComponents(name, button);
+        tab2.addComponents(button);
+        tab3.addComponents(name);
+        tab4.addComponents(button);
+        
+        tabsheet.addTab(tab1, "Overview");
+        tabsheet.addTab(tab2, "Añadir");
+        tabsheet.addTab(tab3, "Modificar");
+        tabsheet.addTab(tab4, "Borrar");
+        
+        //Evento de cambiar pestaña.
+        tabsheet.addSelectedTabChangeListener(event -> {
+        	// Find the tabsheet
+            TabSheet tabsheetActual = event.getTabSheet();
+
+            // Find the tab (here we know it's a layout)
+            Layout tab = (Layout) tabsheetActual.getSelectedTab();
+
+            // Get the tab caption from the tab object
+            String caption = tabsheet.getTab(tab).getCaption();
+
+            // Fill the tab content
+            tab.removeAllComponents();
+        	
+        });
+      
+        //Grid e impresión de layout.
         Grid<Usuario> grid = new Grid<>(Usuario.class);
         grid.setItems(agenda.getContactos());
-
         grid.setColumns("nombre", "ape", "dir", "empresa", "mail", "numero");
-        layout.addComponents(name, button, grid);
+        
+        //Elementos en el layout.
+        layout.addComponents(tabsheet, grid);
         
         setContent(layout);
-        
         
         agenda.mostrarLista();   
     }
